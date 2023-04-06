90f2fefec546bd8ed355fc49cdd6b53b6ac5523b
lawalsegun2025 <lawalsegun2025@gmail.com>
2023-04-06 11:19:29 +0100

    Created the jpg to png converter file

---
+++ 001_jpg_to_png_converter/JPGtoPNGconverter.py
@@ -0,0 +1,10 @@
+import sys 
+import os
+from PIL import Image 
+
+# Grab the first and second arguments
+
+# check if new/ exists, if not, create it
+
+# loop through the first argument (Pokedex), 
+# convert images to png
