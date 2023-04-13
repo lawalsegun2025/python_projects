752a9b01df27a08d61c1406c8ed453300ce20ea8
lawalsegun2025 <lawalsegun2025@gmail.com>
2023-04-13 04:27:28 +0100

    only select links with votes

--- 004_web_scraping/scrape.py
+++ 004_web_scraping/scrape.py
@@ -4,17 +4,19 @@ from bs4 import BeautifulSoup
 res = requests.get("https://news.ycombinator.com/news")
 soup = BeautifulSoup(res.text, 'html.parser')
 links = soup.select('.titleline')
-votes = soup.select('.score')
+subtext = soup.select('.subtext')
 
-def create_custom_hn(links, votes):
+def create_custom_hn(links, subline):
     hn = []
     for idx, item in enumerate(links):
         title = links[idx].getText()
         href = links[idx].find('a').get("href", None)
-        points = int(votes[idx].getText().replace(" points", ""))
-        print(points)
-        hn.append({'title': title, 'link': href})
+        vote = subtext[idx].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(" points", ""))
+            print(points)
+            hn.append({'title': title, 'link': href})
     return hn
 
-create_custom_hn(links, votes)
+create_custom_hn(links, subtext)
 
