5de1b441eb67390bf3de6d998bc2f8b1d6a66749
lawalsegun2025 <lawalsegun2025@gmail.com>
2023-04-15 07:43:51 +0100

    updated

--- 005_web_development/web_server/server.py
+++ 005_web_development/web_server/server.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+print(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template("index.html")
+
+@app.route("/about.html")
+def about():
+    return render_template("about.html")
+
+@app.route("/blog")
+def blog():
+    return "<p>These are my thoughts on blog</p>" 
+
+@app.route("/blog/2020/dogs")
+def blog2():
+    return "<p>this is my dog</p>"
